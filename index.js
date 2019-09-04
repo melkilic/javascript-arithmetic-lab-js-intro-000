141e9b01fc8d4a845f5a7dde8de5ff307fd2abb9
Melike KILIC <melike180400@gmail.com>
2019-09-04 20:25:46 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,49 @@
+
+function add(first, second){
+  return  first+second;
+
+}
+alert(add(1,80));
+
+function subtract(first, second){
+  return  first-second;
+
+}
+alert(subtract(60,40));
+
+function multiply(first, second){
+  return  first*second;
+
+}
+alert(multiply(2,3.4));
+
+function divide(first, second){
+  return  first/second;
+
+}
+alert(divide(5.0,2.5));
+
+
+
+var number = 10;
+function(number++){};
+
+// function inc(number) {
+//   number += 5
+// }
+// function dec(number) {
+//   number /= 3
+// }
+// dec()
+//
+// console.log(number)
+// inc()
+//
+// console.log(number)
+
+// makeInt(first,second){
+//   return first;
+//
+// }
+// alert()
+add5()
